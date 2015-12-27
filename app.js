04029836143f043aba5553bdc7fecc67bd952e66
michati <michati001@yahoo.com>
2015-12-27 18:15:40 -0500

    Pushing today's code

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 /**
  * Created by michati on 12/27/15.
  */
+
+module.exports = require('./lib/express');
--- test/heartbeat.js
+++ test/heartbeat.js
@@ -1,3 +1,28 @@
 /**
  * Created by michati on 12/27/15.
  */
+var app = require('../app'),
+    request = require('supertest');
+
+describe('tig-coord heartbeat api', function(){
+    describe('when requesting resource /heartbeat', function(){
+        it('should respond with 200', function(done){
+            request(app)
+                .get('/heartbeat')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+});
+
+//404 handling middleware
+describe('tig-coord heartbeat api', function(){
+    describe('when requesting resource /missing', function(){
+        it('should respond with 404', function(done){
+            request(app)
+                .get('/missing')
+                .expect('Content-Type', /json/)
+                .expect(404, done);
+        });
+    });
+});
