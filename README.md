c2084720685deac2408d7d2bb924799ca3adc74f
michati <michati001@yahoo.com>
2015-12-28 10:02:33 -0500

    Added grunt

--- README.md
+++ README.md
@@ -3,13 +3,17 @@ Web app and api using GitHub's api
 
 12/17/15 -- initial setup
 
--- mocha, supertest and heartbet
+-- mocha, supertest and heartbeat
 Run tests: mocha
+Continuous testing: mocha -w
 Start server: npm start
 Check heartbeat: curl -i http://127.0.0.1:3000/heartbeat
 
 -- nconf
 Set up configuration files.  No more hard coding port numbers, URLs, etc.
 
--- winston, and express logger
+-- winston, express logger
+Generates log files.
+
+-- Automation with grunt-cli, grunt, grunt-cafe-mocha, and grunt-env
 
---
+++ gruntfile.js
@@ -0,0 +1,26 @@
+/**
+ * Created by michati on 12/28/15.
+ */
+
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-cafe-mocha');
+    grunt.loadNpmTasks('grunt-env');
+
+    grunt.initConfig({
+        env: {
+            test: { NODE_ENV: 'TEST' }
+        },
+        cafemocha: {
+            test: {
+                src: 'test/*.js',
+                options: {
+                    ui: 'bdd',
+                    reporter: 'spec'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('test', [ 'env:test','cafemocha:test' ]);
+};
